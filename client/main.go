75638a7ed2bdffd269ed63183ee7732172c6f142
tomocy <tomocy.dev@gmail.com>
2018-09-26 11:18:03 +0900

    add client which post-request with the contents of file named contents.txt as text/plain

--- client/main.go
+++ client/main.go
@@ -6,18 +6,21 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	urllib "net/url"
+	"os"
 )
 
 func main() {
 	url := flag.String("url", "http://localhost:8080", "a url for request")
 	flag.Parse()
 
-	values := urllib.Values{
-		"name": {"Name"},
+	file, err := os.Open("contents.txt")
+	if err != nil {
+		log.Println(err)
+		return
 	}
+	defer file.Close()
 
-	resp, err := http.PostForm(*url, values)
+	resp, err := http.Post(*url, "text/plain", file)
 	if err != nil {
 		log.Println(err)
 		return
