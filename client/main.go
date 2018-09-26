9f22a79e1bb632864f657aa92a8a273fade88cbe
tomocy <tomocy.dev@gmail.com>
2018-09-26 11:34:01 +0900

    add client which post-request as multipart/form-data with the contents of file named contents.txt as plain/text

--- client/main.go
+++ client/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"net/textproto"
 	"os"
 )
 
 func main() {
 	url := flag.String("url", "http://localhost:8080", "a url for request")
 	flag.Parse()
+
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Type", "text/plain")
+	h.Set("Content-Disposition", `form-data; name="message": filename="contents.txt"`)
+	fileWriter, err := w.CreatePart(h)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	file, err := os.Open("contents.txt")
 	if err != nil {
@@ -20,7 +35,10 @@ func main() {
 	}
 	defer file.Close()
 
-	resp, err := http.Post(*url, "text/plain", file)
+	io.Copy(fileWriter, file)
+	w.Close()
+
+	resp, err := http.Post(*url, w.FormDataContentType(), &b)
 	if err != nil {
 		log.Println(err)
 		return
